4c61ed257ed58ed552d983c9e4eb53d3da45ae0b
mamad <30498676+s33mo@users.noreply.github.com>
2025-02-20 19:38:38 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -108,7 +108,7 @@ CLOUDFLARED_ADDRESS=$cloudflared_address
 VLESS=$vless
 EOF
 
-    cat >$base_dir/../public_html/sub.txt <<EOF
+    cat >$base_dir/../public_nodejs/public/sub.txt <<EOF
 $vless
 EOF
 
@@ -149,63 +149,16 @@ EOF
 }
 EOF
 
-    cat >$base_dir/../public_html/fragment.json <<EOF
+    cat >$base_dir/../public_nodejs/public/fragment.json <<EOF
 {
   "dns": {
-    "hosts": {
-      "domain:googleapis.cn": "googleapis.com",
-      "dns.alidns.com": [
-        "223.5.5.5",
-        "223.6.6.6",
-        "2400:3200::1",
-        "2400:3200:baba::1"
-      ],
-      "one.one.one.one": [
-        "1.1.1.1",
-        "1.0.0.1",
-        "2606:4700:4700::1111",
-        "2606:4700:4700::1001"
-      ],
-      "dot.pub": [
-        "1.12.12.12",
-        "120.53.53.53"
-      ],
-      "dns.google": [
-        "8.8.8.8",
-        "8.8.4.4",
-        "2001:4860:4860::8888",
-        "2001:4860:4860::8844"
-      ],
-      "dns.quad9.net": [
-        "9.9.9.9",
-        "149.112.112.112",
-        "2620:fe::fe",
-        "2620:fe::9"
-      ],
-      "common.dot.dns.yandex.net": [
-        "77.88.8.8",
-        "77.88.8.1",
-        "2a02:6b8::feed:0ff",
-        "2a02:6b8:0:1::feed:0ff"
-      ],
-      "dns.403.online": [
-        "10.202.10.202",
-        "10.202.10.102"
-      ],
-      "free.shecan.ir": [
-        "178.22.122.100",
-        "185.51.200.2"
-      ]
-    },
+    "hosts": { },
     "servers": [
-      "https://94.140.14.14/dns-query",
-      "https://free.shecan.ir/dns-query",
-      "https://dns.403.online/dns-query",
-      "10.202.10.11",
-      "10.202.10.10",
-      "85.15.1.15",
-      "85.15.1.14",
-      "1.1.1.1"
+      " https://dnsforge.de/dns-query",
+      "176.9.93.198",
+      "176.9.1.117",
+      "2a01:4f8:151:34aa::198",
+      "2a01:4f8:141:316d::117"
     ],
     "tag": "dns"
   },
